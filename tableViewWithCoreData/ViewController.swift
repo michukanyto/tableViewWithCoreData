b9f6da60a04c7797251e0cffa7cf9b84b2674576
Marlon <escobar.marlon@gmail.com>
2019-04-30 10:16:58 -0400

    createPerson method created

--- tableViewWithCoreData/ViewController.swift
+++ tableViewWithCoreData/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    let persistenceManager: PersistenceManager
+    
+    init(persistenceManager: PersistenceManager ){//Dependecy Injection
+        self.persistenceManager = persistenceManager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createPerson()
+    }
+    
+   //Create CoreData object
+    func createPerson(){
+//        let person = Person(name: "Jaime", email: "jaime@gmail.com", phone: 563746543)
+        let person = Person(context: persistenceManager.context)
+        person.name = "Jaime"
+        person.email = "jaime@gmail.com"
+        person.phone = 563746543
+        
+        persistenceManager.saveContext()
+        
+    }
+    
+    func getPeople(){
+        guard let people = try! persistenceManager.context.fetch(Person.fetchRequest()) as? [Person] else { return }
+        people.forEach({print($0.name)})
     }
 
 
